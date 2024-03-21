e0e703f249fd449718871e337b55067b68c7b3e2
SAMI <SAMI@DESKTOP-95N033J>
2024-03-21 23:24:40 +0500

    making class course

--- university management system/lab7.cpp
+++ university management system/lab7.cpp
@@ -70,4 +70,85 @@ public:
     {
         return teacherID;
     }
+}; 
+class Course
+{
+private:
+    string courseCode;
+    string courseName;
+    Teacher teacher;
+    vector<string> studentsEnrolled;
+    int maxCapacity;
+    int currentCapacity;
+
+public:
+    Course(string code, string name, Teacher t, int capacity)
+    {
+        courseCode = code;
+        courseName = name;
+        teacher = t;
+        maxCapacity = capacity;
+        currentCapacity = 0;
+    }
+
+    bool addStudent(string student)
+    {
+        if (currentCapacity >= maxCapacity)
+        {
+            cout << "Course is full. Cannot enroll more students." << endl;
+            return false;
+        }
+
+        studentsEnrolled.push_back(student);
+        currentCapacity++;
+
+        return true;
+    }
+
+    void removeStudent(string student)
+    {
+        for (int i = 0; i < studentsEnrolled.size(); ++i)
+        {
+            if (studentsEnrolled[i] == student)
+            {
+                studentsEnrolled.erase(studentsEnrolled.begin() + i);
+                currentCapacity--;
+                return;
+            }
+        }
+    }
+
+    void viewStudents()
+    {
+        int index = 1;
+        cout << endl << "Students Enrolled in " << courseName << " are mentioned below " << endl << endl;
+        for (string student : studentsEnrolled)
+        {
+            cout << "\t\t\t\t Student " << index << " : " << student << endl;
+            index++;
+        }
+        cout << endl << endl;
+    }
+
+    void saveToFile(ofstream& file)
+    {
+        file << courseCode << "," << courseName << "," << teacher.getTeacherID() << "," << currentCapacity << "," << maxCapacity << ",";
+
+        for (string student : studentsEnrolled)
+        {
+            file << student << ";";
+        }
+
+        file << endl;
+    }
+
+    string getCourseCode()
+    {
+        return courseCode;
+    }
+
+    string getCourseName()
+    {
+        return courseName;
+    }
 };
